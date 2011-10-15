56c905abecf12721632fec0dc8d0576a2fbdd51a
Brian Auton <brianauton@gmail.com>
2011-10-15 16:13:58 -0400

    Test fixes

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,7 @@
 require 'rubygems'
 require 'cucumber/rails'
+require 'database_cleaner/cucumber'
 Capybara.default_driver = :webkit
 Capybara.default_selector = :css
 ActionController::Base.allow_rescue = false
-begin
-  DatabaseCleaner.strategy = :transaction
-rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-end
+DatabaseCleaner.strategy = :truncation
--- test/functional/sponsor_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SponsorControllerTest < ActionController::TestCase
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-end
