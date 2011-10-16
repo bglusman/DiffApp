5bd7b07f3cc8e10743f021b98abf431a2cf2b56a
Brian Glusman <brian@neomindlabs.com>
2011-10-15 22:31:42 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+cause = Cause.create :name => "Kids"
 action = Action.create :name => "help with reading difficulties.", :description => "Teachers who are experts in learning difficulties help with task and puzzle design."
 Incentive.create  :action_id => action.id,
+                  :cause_id => cause.id,
                   :name => "give you a massage.",
                   :description => "You can come to my studio for a complimentary 30 minute massage, must be local"
 
 
+cause = Cause.create :name => "Bike Church"
 action = Action.create  :name => "used bike parts.",
+                        :cause_id => cause.id,
                         :description => "Philadelphia Neighborhood Bikeworks runs the Bike 'Church' " +
                           "and needs bikes and spare parts to repair and " +
                           "provide to low income families for environmentally friendly transportation"
@@ -22,23 +25,29 @@ Incentive.create  :action_id => action.id,
                   :description => "If you're local, I'll get a beer with you.  If not, you can grab it " +
                     "when you're in town or I can call your local bar and pick up the tab!"
 
-action = Action.create :name => "", :description => ""
-Incentive.create :action_id => action.id, :name => ""
+cause = Cause.create :name => "Rails"
+action = Action.create  :name => "programming help",
+                        :cause_id => cause.id,
+                        :description => "There's a lot of bugs in the latest release candidate.  Pick one and fix it!"
+Incentive.create  :action_id => action.id,
+                  :cause_id => cause.id,
+                  :name => "donate $20 to the charity of your choice.",
+                  :description => "Let us know the charity and we'll give the money in your name.  We'd also love to hear how you fixed the bug!"
 
-action = Action.create :name => "", :description => ""
-Incentive.create :action_id => action.id, :name => ""
+cause = Cause.create :name => "Montgomery County, PA"
+action = Action.create  :name => "help tutoring illiterate adults",
+                        :cause_id => cause.id,
+                        :description => "10% of adults are not fully literate, and volunteers can help them become confident and functional reading and writing!"
+#Incentive.create  :action_id => action.id,
+#                  :name => ""
+#                  :description => ""
 
-action = Action.create :name => "", :description => ""
-Incentive.create :action_id => action.id, :name => ""
+# cause = Cause.create :name => ""
+# action = Action.create  :name => "",
+#                         :cause_id => cause.id,
+#                         :description => ""
+# Incentive.create  :action_id => action.id,
+#                   :name => ""
+#                   :description => ""
 
-sponsor = Sponsor.create :name => "Actionarium"
-action = Action.create :sponsor_id => sponsor.id, :name => "Start a company"
-Incentive.create :action_id => action.id, :name => "20 minutes of designer time", :description => "These are the details of the incentive.", :sponsor_id => sponsor.id
 
-sponsor = Sponsor.create :name => "ForProfit LLC"
-action = Action.create :sponsor_id => sponsor.id, :name => "Contribute to Rails"
-Incentive.create :action_id => action.id, :name => "Free food", :description => "We'll give you some food", :sponsor_id => sponsor.id
-
-sponsor = Sponsor.create :name => "NonProfit Inc"
-action = Action.create :sponsor_id => sponsor.id, :name => "Teach a cooking class"
-Incentive.create :action_id => action.id, :name => "Free beer", :description => "We'll give you some beer", :sponsor_id => sponsor.id
