a666a6f9d5b6dbc0c4a0bbf13f6b8d8544795923
Brian Glusman <brian@neomindlabs.com>
2011-10-15 23:42:12 -0400

    fixed seeds and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,41 @@
 #   Mayor.create!(name: 'Emanuel', city: cities.first)
 
 cause = Cause.create! :name => "Kids"
+supporter = Actor.create! :name => "Ruzzle"
 action = Action.create!  :name => "help with reading difficulties.",
                         :cause_id => cause.id,
                         :description => "Teachers who are experts in learning difficulties help with task and puzzle design."
-Incentive.create!  :action_id => action.id,
-                  :name => "give you a massage.",
-                  :description => "You can come to my studio for a complimentary 30 minute massage, must be local"
+Incentive.create!   :action_id => action.id,
+                    :supporter_id => supporter.id,
+                    :name => "give you a massage.",
+                    :description => "You can come to my studio for a complimentary 30 minute massage, must be local"
 
 
 cause = Cause.create! :name => "Bike Church"
+supporter = Actor.create! :name => "Brian G."
 action = Action.create!  :name => "used bike parts.",
                         :cause_id => cause.id,
                         :description => "Philadelphia Neighborhood Bikeworks runs the Bike 'Church' " +
                           "and needs bikes and spare parts to repair and " +
                           "provide to low income families for environmentally friendly transportation"
-Incentive.create!  :action_id => action.id,
-                  :name => "buy you a beer.",
-                  :description => "If you're local, I'll get a beer with you.  If not, you can grab it " +
+Incentive.create!   :action_id => action.id,
+                    :supporter_id => supporter.id,
+                    :name => "buy you a beer.",
+                    :description => "If you're local, I'll get a beer with you.  If not, you can grab it " +
                     "when you're in town or I can call your local bar and pick up the tab!"
 
 cause = Cause.create! :name => "Rails"
-action = Action.create!  :name => "programming help",
-                        :cause_id => cause.id,
-                        :description => "There's a lot of bugs in the latest release candidate.  Pick one and fix it!"
-Incentive.create!  :action_id => action.id,
-                  :name => "donate $20 to the charity of your choice.",
-                  :description => "Let us know the charity and we'll give the money in your name.  We'd also love to hear how you fixed the bug!"
+supporter = Actor.create! :name => "Neomind Labs"
+action = Action.create!   :name => "programming help",
+                          :cause_id => cause.id,
+                          :description => "There's a lot of bugs in the latest release candidate.  Pick one and fix it!"
+Incentive.create!   :action_id => action.id,
+                    :supporter_id => supporter.id,
+                    :name => "donate $20 to the charity of your choice.",
+                    :description => "Let us know the charity and we'll give the money in your name.  We'd also love to hear how you fixed the bug!"
 
 cause = Cause.create! :name => "Montgomery County, PA"
+supporter = Actor.create! :name => "RSVP of Montgomery County"
 action = Action.create!  :name => "help tutoring illiterate adults",
                         :cause_id => cause.id,
                         :description => "10% of adults are not fully literate, and volunteers can help them become confident and functional reading and writing!"
