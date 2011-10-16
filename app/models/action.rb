9d18cac748d63990cd0fc5bbf6ab7d6ab96a501d
Brian Glusman <brian@neomindlabs.com>
2011-10-15 22:57:35 -0400

    index text and link changes

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,5 +1,6 @@
 class Action < ActiveRecord::Base
   has_many :incentives
 
+  belongs_to :cause
   validates :name, :presence => true
 end
--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,10 +1,10 @@
 class Incentive < ActiveRecord::Base
-  belongs_to :sponsor
   belongs_to :action
   belongs_to :claimant, :class_name => "Actor", :foreign_key => :claimant_id
   belongs_to :supporter, :class_name => "Actor", :foreign_key => :supporter_id
 
   validates :name, :presence => true
+  validates :supporter, :presence => true
 
   def provider
     supporter || sponsor
