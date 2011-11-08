0b0ccbbb0e0c31774aed254fd3420afbc4901fef
Brian Glusman <brian@neomindlabs.com>
2011-11-08 10:20:42 -0500

    fixing omniauth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    actor = Actor.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Actor.create_with_omniauth(auth, request.remote_ip)
+    actor = Actor.find_by_provider_and_uid(auth_hash["provider"], auth["uid"]) || Actor.create_with_omniauth(auth_hash, request.remote_ip)
     session[:actor_id] = actor.id
     redirect_to root_url, :notice => "Signed in!"
   end
@@ -9,6 +9,12 @@ class SessionsController < ApplicationController
   def destroy
     session[:actor_id] = nil
     redirect_to root_url, :notice => "Signed out!"
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
 end
